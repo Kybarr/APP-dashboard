d79f9efefe4b6f40bde4e7df9d900ae2062e64b6
Kybarr <129209479+Kybarr@users.noreply.github.com>
2025-03-08 21:31:49 +0700

    Dashboard.py

--- Dashboard/Dashboard.py
+++ Dashboard/Dashboard.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
+import plotly.express as px
 
 img = Image.open('Dashboard/sepeda-gunung.jpg')
 st.sidebar.image(img)
